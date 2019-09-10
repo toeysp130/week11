2877356f4a406e2d35e5e0e809daf86660112edf
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2019-09-10 14:19:47 +0700

    Create w112.cpp

---
+++ w112.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+/*int main()
+{
+	int a[4] = {-1,6,9,2};
+	int min = a[0];
+	for(int i = 1;i < 4;i++)
+	{
+		if(a[i] < min)
+		min = a[i];
+	}
+	cout << "Manimum value is " << min << endl;
+	system("pause");
+	return(0);
+}*/
