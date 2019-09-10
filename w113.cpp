f8e0b0d8d1fd032bd28ca0a32a70e6bba417b319
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2019-09-10 14:19:49 +0700

    Create w113.cpp

---
+++ w113.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+/*int main()
+{
+	int a[10] = {99,19,1,23,0,15,10,23,-26,-9};
+	int temp;
+	for(int i = 0;i < 9;i++)
+		for(int i = 0;i < 9;i++)
+		{
+			if (a[i] > a[i+1]){
+				temp = a[i+1];
+				a[i+1] = a[i];
+				a[i] = temp;
+			}
+		}
+	for(int i = 0;i <=9;i++)
+	{
+		cout << a[i] << " ";//endl;
+	}
+	cout <<"\nThe maximum number is " << a[9] << endl;
+	cout <<"\nThe maximum number is " << a[0] << endl;
+	system("pause");
+	return(0);
+}*/
